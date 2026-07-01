d20e6bb507e17d96b1ba77987349d3a86ba0ec6f
victor araujo <email.victornascimento@gmail.com>
2026-07-01 15:14:10 -0300

    extrai a logica do calculo para a função e adiciona loop de validação

--- src/calculadora.java
+++ src/calculadora.java
@@ -3,31 +3,46 @@ import java.util.Scanner;
 public class calculadora {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
+        calcularNumeros(entrada);
+        entrada.close();
+    }
 
-        System.out.println("Digite um número: ");
-        int numero1 = entrada.nextInt();
-        System.out.println("Digite outro número");
-        int numero2 = entrada.nextInt();
+    public static double calcularNumeros(Scanner entrada) {
+        System.out.println("Digite um numero");
+        double numero1 = entrada.nextDouble();
+        System.out.println("Digite outro numero");
+        double numero2 = entrada.nextDouble();
 
-        System.out.println("Digite (1) para somar os numeros, (2) para subtrair, (3) para dividir e (4) para multiplicar");
-        int numeroDigitado = entrada.nextInt();
+
 
         double soma = numero1 + numero2;
         double subtracao = numero1 - numero2;
         double divisao = numero1 / numero2;
         double multiplicacao = numero1 * numero2;
+        boolean opcaoFalsa = false;
+
+        while (!opcaoFalsa) {
+            System.out.println("Digite (1) para somar os numeros, (2) para subtrair, (3) para dividir e (4) para multiplicar");
+            double numeroDigitado = entrada.nextDouble();
 
         if (numeroDigitado == 1) {
             System.out.println("O resultado da soma: " + soma);
+            opcaoFalsa = true;
         } else if (numeroDigitado == 2) {
             System.out.println("O resultado da subtração: " + subtracao);
+            opcaoFalsa = true;
         } else if (numeroDigitado == 3) {
             System.out.println("O resultado da divisão: " + divisao);
+            opcaoFalsa = true;
         } else if (numeroDigitado == 4) {
             System.out.println("O resultado da multiplicação: " + multiplicacao);
+            opcaoFalsa = true;
         } else {
-            System.out.println("Numero inválido.");
+            System.out.println("O numero digitado é invalido, digite novamente.");
+        }
         }
 
+        return 0;
     }
+
 }
